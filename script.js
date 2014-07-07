2b90777bb2e1aa73900cdd747f8134893ffd2ad8
hujuu <kenakai@a5.keio.jp>
2014-07-08 00:16:09 +0900

    add javascript code

--- script.js
+++ script.js
@@ -25,3 +25,25 @@ Particle.prototype = {
 	radius: 0,
 	color: null,
 	isRemove: false,
+	//初速度、サイズ、色をランダムに設定
+	create: function(){
+		this.vx = Math.random() * 6 - 3;
+		this.vy = Math.random() * (-6) - 2;
+		this.radius = Math.random() * 20 + 5;
+		var r = 150;
+		var g = Math.floor(Math.random() * 100 + 155);
+		var b = Math.floor(Math.random() * 155 + 100);
+		this.color = "rgb(" + r + "," + g + "," + b + ")";
+	},
+	//位置を更新
+	update: function(){
+		this.vy += GRAVITY;
+		this.x += this.vx;
+		this.y += this.vy;
+		this.radius *= .97;
+
+		//パーティクルが画面の外に出たとき削除をフラグをたてる
+		if(this.x < 0 || this.x > SCREEN_WIDTH || this.y > SCREEN_HEIGHT){
+			this.isRemove = true;
+		}
+	},
